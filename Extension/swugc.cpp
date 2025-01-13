c7513cf1eb4ab8064aa9c120a20316e727ac7177
Riley S. <substitute@pocketdevs.org>
2025-01-13 09:10:36 -0500

    Continued Work

--- Extension/swugc.cpp
+++ Extension/swugc.cpp
@@ -18,6 +18,10 @@
 
 #include "swugc.h"
 #include <cstdio>
+#include <filesystem>
+#include <iostream>
+
+bool has_init = false;
 
 static ISteamUGC* GetUGCPointer(void)
 {
@@ -26,6 +30,9 @@ static ISteamUGC* GetUGCPointer(void)
 
 static cell_t sm_TestUGC(IPluginContext* pContext, const cell_t* params)
 {
+    if (!has_init)
+        return has_init;
+
     ISteamUGC* pUGC = GetUGCPointer();
     printf("Running\n");
 	if (pUGC == NULL)
@@ -34,14 +41,66 @@ static cell_t sm_TestUGC(IPluginContext* pContext, const cell_t* params)
 		return BAD_HANDLE;
 	}
     printf("Downloading\n");
-    pUGC->BInitWorkshopForGameServer(550, R"(C:\Users\Admin\test_workshop)");
-    pUGC->DownloadItem(3404846348, true);
+    pUGC->DownloadItem(3404846348, false);
     return 1;
+}
+
+static cell_t sm_InitWorkshopForGameServer(IPluginContext* pContext, const cell_t* params)
+{
+    if (has_init)
+        return has_init;
+
+    const auto ugc = GetUGCPointer();
+    if (!ugc)
+        return false;
+
+	char* location;
+    const DepotId_t depot = params[1];
+    pContext->LocalToString(params[2], &location);
+
+    char realpath[PLATFORM_MAX_PATH];
+    g_pSM->BuildPath(Path_Game, realpath, sizeof(realpath), "%s", location);
+
+    printf("Directory is %s\n", realpath);
+
+    printf("As Path(): %s\n", std::filesystem::path(realpath).string().c_str());
+
+    has_init = ugc->BInitWorkshopForGameServer(depot, realpath);
+    return has_init;
+}
+
+static cell_t sm_DownloadItem(IPluginContext* pContext, const cell_t* params)
+{
+    if (!has_init)
+        return has_init;
+
+    const auto ugc = GetUGCPointer();
+    if (!ugc)
+        return false;
+
+    char* item;
+    pContext->LocalToString(params[1], &item);
+	const auto item_id = std::stoull(item);
+    printf("User has request to download the following item %llu\n", item_id);
+	return ugc->DownloadItem(item_id, params[2]);
+}
+
+static cell_t sm_CreateHardlink(IPluginContext* pContext, const cell_t* params)
+{
+    char* source;
+    char* dest;
+    pContext->LocalToString(params[1], &source);
+    pContext->LocalToString(params[2], &dest);
+    std::filesystem::create_hard_link(source, dest);
+    return 0;
 }
 
 
 static sp_nativeinfo_t ugcnatives[] = {
-	{"SteamWorks_TestUGC",				sm_TestUGC},
+    {"SteamWorks_TestUGC", sm_TestUGC},
+    {"CreateHardlink", sm_CreateHardlink},
+    {"SteamWorks_InitWorkshopForGameServer", sm_InitWorkshopForGameServer},
+    {"SteamWorks_UGC_DownloadItem", sm_DownloadItem},
 	{NULL,											NULL}
 };
 
